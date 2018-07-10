b65a3a37268643ad6c804436fd9bfaf3c4067ab7
Latifat Ozoya <latifat@fb.com>
2018-07-10 14:51:02 -0700

    camera

--- app/src/main/java/com/app/latifat/parstagram/HomeActivity.java
+++ app/src/main/java/com/app/latifat/parstagram/HomeActivity.java
@@ -1,19 +1,28 @@
 package com.app.latifat.parstagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.app.latifat.parstagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import java.io.File;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -27,14 +36,12 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //descriptionInput = findViewById(R.id.descriprtion_et);
-        //createButton = findViewById(R.id.create_btn);
-        //refreshButton = findViewById(R.id.refresh_btn);
-
         loadTopPosts();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         logoutBtn = (Button) findViewById(R.id.logout_btn);
+        descriptionInput = (EditText) findViewById(R.id.description_et);
+        createButton = (Button) findViewById(R.id.create_btn);
 
         setSupportActionBar(toolbar);
 
@@ -47,9 +54,10 @@ public class HomeActivity extends AppCompatActivity {
                 final Intent intent = new Intent(HomeActivity.this,MainActivity.class);
                 startActivity(intent);
             }
-
         });
 
+        View view = findViewById(R.id.home);
+        onLaunchCamera(view);
     }
 
     private void loadTopPosts() {
@@ -73,5 +81,54 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo";
+    File photoFile;
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
 }
